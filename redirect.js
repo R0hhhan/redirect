39072d0c4b1cbbce6b24fa5d44cb7beef34a1e2c
r0hhhan <rohanpatil3343@gmail.com>
2025-03-06 02:00:18 +0530

    added back

--- redirect.js
+++ redirect.js
@@ -4,13 +4,26 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
+
+let auth_code = -1;
+
 app.get("/", (req, res) => {
 
     const auth = req.query.auth_code;
     console.log(auth);
     res.send("hey there we are active")
+
+    auth_code = auth;
+
+    setTimeout(() => {
+        auth_code = -1;
+    }, 1000000);
 });
 
+
+app.get("/back", (req, res) => {
+    res.json({ auth: auth });
+});
 app.listen(3000, "0.0.0.0", () => {
     console.log("Server running on port 3000");
 });
