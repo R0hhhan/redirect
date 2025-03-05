9abea7386ddb7f86933ba8fa0b861cdc83be4054
r0hhhan <rohanpatil3343@gmail.com>
2025-03-06 01:48:55 +0530

    corrected

--- redirect.js
+++ redirect.js
@@ -8,9 +8,6 @@ app.get("/", (req, res) => {
 
     const auth = req.query.auth_code;
     console.log(auth);
-    console.log(req.query);
-    console.log(req.body);
-    console.log(req.params);
     res.send("hey there we are active")
 });
 
